9fb66b46847bd60bea49f2c0de4caded8cf3fff2
Mubashir Jilani <nk7674356@gmail.com>
2025-01-09 13:25:38 +0500

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+var toDoList = []
+
+toDoList.push("Task1", "Task2", "Task3")
+toDoList.unshift("Task4", "Task5")
+
+console.log(toDoList)
+
+toDoList.pop()
+console.log(toDoList)
+
+toDoList.shift()
+console.log(toDoList)
